cb69d89e2590114fe4b09755a74b12eb90370698
NorahSketch <norahmariano@gmail.com>
2021-12-07 16:52:22 +0100

    Process Testing

--- src/app/API.ts
+++ src/app/API.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // TODO: Set url
 
-const baseURL = ''
+// const baseURL = 'https://server-gi.herokuapp.com/'
+const baseURL = process.env.REACT_APP_API_URL
 // const baseURL = 'http://localhost:5005/'
 
 const API = axios.create({ baseURL, withCredentials: true })
