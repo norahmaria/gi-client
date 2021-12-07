474936d757b25d416eb0067a7875218cb32531af
NorahSketch <norahmariano@gmail.com>
2021-12-07 23:31:41 +0100

    Remove sign in flicker on load

--- src/app/API.ts
+++ src/app/API.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // TODO: Set url
 
-// const baseURL = 'https://server-gi.herokuapp.com/'
-const baseURL = 'http://localhost:5005/'
+const baseURL = 'https://server-gi.herokuapp.com/'
+// const baseURL = 'http://localhost:5005/'
 
 const API = axios.create({ baseURL, withCredentials: true })
 
