e49b6fd6b0af51331920a72b35bbe8fc299095c4
NorahSketch <norahmariano@gmail.com>
2021-12-07 17:30:15 +0100

    API Url

--- src/app/API.ts
+++ src/app/API.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // TODO: Set url
 
-// const baseURL = 'https://server-gi.herokuapp.com/'
-const baseURL = process.env.REACT_APP_API_URL
+const baseURL = 'https://server-gi.herokuapp.com/'
+// const baseURL = process.env.REACT_APP_API_URL
 // const baseURL = 'http://localhost:5005/'
 
 const API = axios.create({ baseURL, withCredentials: true })
