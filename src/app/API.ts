796f781438ae8588e45f874d1536ad7d5615cae2
NorahSketch <norahmariano@gmail.com>
2021-12-09 14:36:26 +0100

    Minor style changes

--- src/app/API.ts
+++ src/app/API.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // TODO: Set url for socket and api when in local host
 
-// const baseURL = 'https://server-gi.herokuapp.com/'
-const baseURL = 'http://localhost:5005/'
+const baseURL = 'https://server-gi.herokuapp.com/'
+// const baseURL = 'http://localhost:5005/'
 
 const API = axios.create({ baseURL, withCredentials: true })
 
