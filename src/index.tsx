974c59ad51644a084af759677beaee2f7d03a9dc
NorahSketch <norahmariano@gmail.com>
2021-12-10 23:39:53 +0100

    Read Me

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
+import env from 'dotenv'
 
 import { QueryProvider } from './context/Query'
 import { SocketProvider } from './context/Socket'
 import { UserProvider } from './context/User'
 import { BrowserRouter } from 'react-router-dom'
+
+env.config()
 
 ReactDOM.render(
   <React.StrictMode>
